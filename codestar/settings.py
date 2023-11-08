b27453bc8fc5ea35fbc63344383aeae9020dcbd0
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-11-08 21:58:03 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blogcodestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blogcodestar-f0096818438c.herokuapp.com', 'localhost']
 
 
 # Application definition
